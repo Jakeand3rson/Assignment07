a2017a3c0df95b40ebfec04aff8d01f52ab6913e
Jakealope <Jakealope@gmail.com>
2014-03-13 10:57:49 -0700

    Assignment07
    this is a project that links "ServiceClient" to a database.

---
+++ App_Code/ICommunityAssistServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ICommunityAssistServices
+/// </summary>
+public class ICommunityAssistServices
+{
+	public ICommunityAssistServices()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
